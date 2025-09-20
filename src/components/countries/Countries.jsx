8586494bdf68d88e2c720b5a33bba5f940be19a9
kamrulhasankanan <kamrulhasankanan@gmail.com>
2025-09-20 15:51:20 +0600

    added countries

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { use } from "react";
+import Country from "../Country/Country";
+import "./Countries.css";
 
-const Countries = () => {
-    return (
-        <div>
-            <h1></h1>
-        </div>
-    );
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
+  const countries = countriesData.countries;
+
+  return (
+    <div>
+      <p>Countries: {countries.length}</p>
+      <div className="countries" >
+        {countries.map((country) => (
+          <Country 
+            key={country.cca3.cca3} 
+            country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Countries;   
+export default Countries;
---
+++ src/components/country/Country.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import './Country.css'
+const Country = ({country}) => {
+    const [visited, setVisited] = useState(false);
+    // console.log(country.area.area)
+
+    const handleVisited = () => {
+        setVisited(!visited)
+    }
+
+    return (
+        <div className={`country ${visited && 'country-visited'}`}>
+            <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
+            <h3>Name: {country.name.common} </h3>
+            <p>Population: {country.population.population}</p>
+            <p>Region: {country.region.region}</p>
+            <p>Area: {country.area.area} {country.area.area > 300000 ? "Big Country" : "Small Country"}</p>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
+        </div>
+    );
+};
+
+export default Country;
